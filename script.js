fd55aa2dd72a6f99e2d2205321d586debb5a1594
Ka Long Ngai <kls8162@gmail.com>
2022-12-29 13:48:38 +0800

    Update 11, WorkPage Image links.

--- script.js
+++ script.js
@@ -7,27 +7,28 @@ function updateProjectDisplay() {
     document.getElementById("projectName").innerHTML = projects[currentProjectIndex].Name;
     document.getElementById("projectDescription").innerHTML = projects[currentProjectIndex].Description;
     document.getElementById("projectImages").src = projects[currentProjectIndex].Image;
-}s
+    document.getElementById("projectLinks").href = projects[currentProjectIndex].Link;
+}
 
 function startWorkWeb() {
-    project1 = { Name: "Slidle", Image: "/img/Github.png", Description: "Slider Puzzle, participated in the Peanut Jam 2022 hosted by TechCodes, Won the Award of Most Friendly UI."};
-    project2 = { Name: "SBU Weather Bot", Image: "/img/Linkedin.png", Description: "A discord command control weather bot that report weather using weather.js, node.js and discord.js."};
-    project3 = { Name: "Portfolio Website", Image: "/img/KLS_2023.png", Description: "A Website where I showcase my journey along the way as a prospect software engineer."};
+    project1 = { Name: "Slidle", Image: "/img/Github.png", Description: "Slider Puzzle, participated in the Peanut Jam 2022 hosted by TechCodes, Won the Award of Most Friendly UI.", Link: "https://github.com/kalongn/PeanutJam2022"};
+    project2 = { Name: "SBU Weather Bot", Image: "/img/Linkedin.png", Description: "A discord command control weather bot that report weather using weather.js, node.js and discord.js.", Link: "https://github.com/kalongn/SBU-Weather-BOT"};
+    project3 = { Name: "Portfolio Website", Image: "/img/KLS_2023.png", Description: "A Website where I showcase my journey along the way as a prospect software engineer.", Link: "https://github.com/kalongn/Portfolio-Website"};
     projects = new Array(project1, project2, project3);
     currentProjectIndex = 0;
     updateProjectDisplay();
 }
 
 function leftArrow() {
-    document.getElementById("indicator"+currentProjectIndex).classList.remove("active");
+    document.getElementById("indicator" + currentProjectIndex).classList.remove("active");
     currentProjectIndex - 1 == -1 ? currentProjectIndex = 2 : currentProjectIndex--;
     updateProjectDisplay();
-    document.getElementById("indicator"+currentProjectIndex).classList.add("active");
+    document.getElementById("indicator" + currentProjectIndex).classList.add("active");
 }
 
 function rightArrow() {
-    document.getElementById("indicator"+currentProjectIndex).classList.remove("active");
+    document.getElementById("indicator" + currentProjectIndex).classList.remove("active");
     currentProjectIndex + 1 == 3 ? currentProjectIndex = 0 : currentProjectIndex++;
     updateProjectDisplay();
-    document.getElementById("indicator"+currentProjectIndex).classList.add("active");
+    document.getElementById("indicator" + currentProjectIndex).classList.add("active");
 }
