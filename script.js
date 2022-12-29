116202688c5cec0ba8f37ad0004d67930cc59ff3
Ka Long Ngai <kls8162@gmail.com>
2022-12-29 13:35:46 +0800

    Update 10, Work Page Done.

---
+++ script.js
@@ -0,0 +1,33 @@
+//WorkPage code.
+let project1, project2, project3;
+let projects;
+let currentProjectIndex;
+
+function updateProjectDisplay() {
+    document.getElementById("projectName").innerHTML = projects[currentProjectIndex].Name;
+    document.getElementById("projectDescription").innerHTML = projects[currentProjectIndex].Description;
+    document.getElementById("projectImages").src = projects[currentProjectIndex].Image;
+}s
+
+function startWorkWeb() {
+    project1 = { Name: "Slidle", Image: "/img/Github.png", Description: "Slider Puzzle, participated in the Peanut Jam 2022 hosted by TechCodes, Won the Award of Most Friendly UI."};
+    project2 = { Name: "SBU Weather Bot", Image: "/img/Linkedin.png", Description: "A discord command control weather bot that report weather using weather.js, node.js and discord.js."};
+    project3 = { Name: "Portfolio Website", Image: "/img/KLS_2023.png", Description: "A Website where I showcase my journey along the way as a prospect software engineer."};
+    projects = new Array(project1, project2, project3);
+    currentProjectIndex = 0;
+    updateProjectDisplay();
+}
+
+function leftArrow() {
+    document.getElementById("indicator"+currentProjectIndex).classList.remove("active");
+    currentProjectIndex - 1 == -1 ? currentProjectIndex = 2 : currentProjectIndex--;
+    updateProjectDisplay();
+    document.getElementById("indicator"+currentProjectIndex).classList.add("active");
+}
+
+function rightArrow() {
+    document.getElementById("indicator"+currentProjectIndex).classList.remove("active");
+    currentProjectIndex + 1 == 3 ? currentProjectIndex = 0 : currentProjectIndex++;
+    updateProjectDisplay();
+    document.getElementById("indicator"+currentProjectIndex).classList.add("active");
+}
