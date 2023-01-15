5786d1134c692d9971a1d3f98e86ba80ffce704f
Ka Long Ngai <kls8162@gmail.com>
2023-01-15 16:48:17 -0500

    Update 30, Prototype ready for Deploy.

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function updateProjectDisplay() {
 }
 
 function startWorkWeb() {
-    project1 = { Name: "Slidle", Image: "/img/Slidle.png", Description: "Slider Puzzle, participated in the Peanut Jam 2022 hosted by TechCodes, Won the Award of Most Friendly UI.", Link: "https://github.com/kalongn/PeanutJam2022"};
-    project2 = { Name: "SBU Weather Bot", Image: "/img/SBU_Bot.png", Description: "A discord command control weather bot that report weather using weather.js, node.js and discord.js.", Link: "https://github.com/kalongn/SBU-Weather-BOT"};
-    project3 = { Name: "Portfolio Website", Image: "/img/KLS_2023.png", Description: "A Website where I showcase my journey along the way as a prospect software engineer.", Link: "https://github.com/kalongn/Portfolio-Website"};
+    project1 = { Name: "Slidle", Image: "/img/Slidle.png", Description: "Slider Puzzle, participated in the Peanut Jam 2022 hosted by TechCodes, Won the Award of Most Friendly UI.", Link: "https://github.com/kalongn/PeanutJam2022" };
+    project2 = { Name: "SBU Weather Bot", Image: "/img/SBU_Bot.png", Description: "A discord command control weather bot that report weather using weather.js, node.js and discord.js.", Link: "https://github.com/kalongn/SBU-Weather-BOT" };
+    project3 = { Name: "Portfolio Website", Image: "/img/KLS_2023.png", Description: "A Website where I showcase my journey along the way as a prospect software engineer.", Link: "https://github.com/kalongn/Portfolio-Website" };
     projects = new Array(project1, project2, project3);
     currentProjectIndex = 0;
     updateProjectDisplay();
