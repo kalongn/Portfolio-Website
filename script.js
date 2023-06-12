70e6de04b96d431d17bb70b93ab087ec7f3ec6d5
Ka Long Ngai <kls8162@gmail.com>
2023-06-11 22:54:24 -0400

    Update: Navbar buttons.

--- script.js
+++ script.js
@@ -11,3 +11,47 @@ mainFrame.onpointermove = event => {
         top: `${clientY - offSet}px`
     }, { duration: 1000, fill: "forwards" });
 }
+
+/**
+ * Add a class to an element.
+ * @param {*} el 
+ *      the html element selected.
+ * @param {*} name 
+ *      the name of the class to be added.
+ */
+function addClass(el, name) {
+    el.classList.add(name);
+}
+
+/**
+ * Toggle a class to an element.
+ * @param {*} el 
+ *      the html element selected.
+ * @param {*} name 
+ *      the name of the class to be toggled.
+ */
+function toggleClass(el, name) {
+    el.classList.toggle(name);
+}
+
+/**
+ * Remove a class from an element.
+ * @param {*} el 
+ *      the html element selected.
+ * @param {*} name 
+ *      the name of the class to be added.
+ */
+function removeClass(el, name) {
+    el.classList.remove(name);
+}
+
+let prevActive = document.getElementById('home');
+function navButton(id) {
+    if (document.getElementById(id).classList.contains('active')) {
+        return false;
+    }
+    removeClass(prevActive, 'active');
+    prevActive = document.getElementById(id);
+    addClass(prevActive, 'active');
+    return false;
+}
